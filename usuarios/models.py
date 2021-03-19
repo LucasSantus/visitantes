d58480d48ff15e0cf62689207d90eec39cc3e3d6
Lucas Santus <leos9877@gmail.com>
2021-03-19 10:54:51 -0300

    implementando models

---
+++ usuarios/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import {
+    BaseUserManager, 
+    AbstractBaseUser, 
+    PermissionsMixin
+}
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    
+    email = models.EmailField(
+        verbose_name = "E-mail do Usuário"
+        max_length=194,
+        unique=True,
+    )
+
+    is_active = models.BooleanField(
+        verbose_name="Usuário está ativo",
+        default=True,
+    )
+
+    is_staff = models.BooleanField(
+        verbose_name="Usuário é da equipe de desenvolvimento",
+        default=False,
+    )
+
+    is_superuser = models.BooleanField(
+        verbose_name="Usuário é um superusuário",
+        default=False,
+    )
+
+    USERNAME_FIELD = "email"
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+        db_table = "usuario"
+
+    def __str__(self):
+        return self.email
